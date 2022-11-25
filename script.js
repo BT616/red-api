9cb4386cf7490cce3b707b83ceef8e709320ec85
BT616 <110855674+BT616@users.noreply.github.com>
2022-11-24 22:11:24 -0800

    item,title, search, print to page

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ searchForm.addEventListener('submit',function(event){
 var search = searchInput.value.trim();
 
 
+
+
+
 var apiURL = `https://api.redcircleapi.com/request?api_key=B53C1783A1D9456488669476C6A2783A&search_term=${search}&type=search`
 fetch(apiURL).then(function(response){
     return response.json();
@@ -17,12 +20,22 @@ fetch(apiURL).then(function(response){
     if(data.length===0){
         alert('product not found')
     }else{
-       
+       var item = data.search_results[0].product
+       var title = data.search_results[0].product.title.trim()
+       var getAll = data.search_results
 
+       var currentItemTitle = document.createElement('h1');
+       var currentContainer = document.getElementById('current')
+       currentItemTitle.textContent= `Search: ${title} `
+       currentContainer.appendChild(currentItemTitle)
+
+       console.log(item)
+console.log(title)
+console.log(getAll)
     }
 })
 
-console.log(apiURL)
+
 })
 
 
